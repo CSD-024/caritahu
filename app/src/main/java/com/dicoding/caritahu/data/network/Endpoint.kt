af78303ac1ca473862912f1cd4d10a99352734d6
Unero <ramadhanbhg@gmail.com>
2021-12-03 13:39:00 +0700

    fix(news-api): Hidden API Key

--- app/src/main/java/com/dicoding/caritahu/data/network/Endpoint.kt
+++ app/src/main/java/com/dicoding/caritahu/data/network/Endpoint.kt
@@ -1,9 +1,14 @@
 package com.dicoding.caritahu.data.network
 
+import com.dicoding.caritahu.BuildConfig.NEWS_KEY
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Endpoint {
-    @GET("top-headlines?country=id&apiKey=509034bcdc454d05a68399a615aaa450")
-    suspend fun topHeadlines(): Response<HeadlinesResponse>
+    @GET("top-headlines")
+    suspend fun topHeadlines(
+        @Query("country") country: String = "id",
+        @Query("apiKey") apiKey: String = NEWS_KEY
+    ): Response<HeadlinesResponse>
 }
