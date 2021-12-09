eecd700caa91ce76b5530cca9197aab8dddcd4ec
Unero <ramadhanbhg@gmail.com>
2021-12-10 00:29:20 +0700

    feat: bottom navigation bar

--- app/src/main/java/com/dicoding/caritahu/MainActivity.kt
+++ app/src/main/java/com/dicoding/caritahu/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dicoding.caritahu
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.dicoding.caritahu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +13,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val botNav = binding.bottomNav
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+        val navController = navHost.navController
+
+        botNav.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener {_, destination, _ ->
+            when(destination.id) {
+                R.id.newsFragment -> botNav.visibility = View.VISIBLE
+                R.id.hoaxFragment -> botNav.visibility = View.VISIBLE
+                R.id.bookmarkFragment -> botNav.visibility = View.VISIBLE
+                else -> botNav.visibility = View.GONE
+            }
+        }
     }
 }
