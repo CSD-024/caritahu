7c91e4065376b33156a701a60cdbaf7e067f1c13
Unero <ramadhanbhg@gmail.com>
2021-12-09 09:16:47 +0700

    feat(api): add endpoint and repository function

--- app/src/main/java/com/dicoding/caritahu/data/Repository.kt
+++ app/src/main/java/com/dicoding/caritahu/data/Repository.kt
@@ -1,11 +1,34 @@
 package com.dicoding.caritahu.data
 
 import com.dicoding.caritahu.data.network.Client
-import com.dicoding.caritahu.data.network.model.Article
+import com.dicoding.caritahu.data.network.model.HoaxArticle
+import com.dicoding.caritahu.data.network.model.HoaxAuthor
+import com.dicoding.caritahu.data.network.model.NewsArticle
+import com.dicoding.caritahu.data.network.model.NewsSource
 import retrofit2.Response
 
 object Repository {
-    suspend fun topHeadlines(): Response<List<Article>> {
-        return Client.retrofit.topHeadlines()
+    suspend fun topHeadlines(): Response<List<NewsArticle>> {
+        return Client.retrofit.newsHeadlines()
+    }
+
+    suspend fun searchNews(query: String): Response<List<NewsArticle>> {
+        return Client.retrofit.newsSearch(query)
+    }
+
+    suspend fun newsSource(): Response<List<NewsSource>> {
+        return Client.retrofit.newsSources()
+    }
+
+    suspend fun latestHoax(): Response<List<HoaxArticle>> {
+        return Client.retrofit.hoaxLatest()
+    }
+
+    suspend fun searchHoax(query: String): Response<List<HoaxArticle>> {
+        return Client.retrofit.hoaxSearch(query)
+    }
+
+    suspend fun hoaxAuthor(): Response<List<HoaxAuthor>> {
+        return Client.retrofit.hoaxAuthors()
     }
 }
--- app/src/main/java/com/dicoding/caritahu/data/network/Endpoint.kt
+++ app/src/main/java/com/dicoding/caritahu/data/network/Endpoint.kt
@@ -1,10 +1,29 @@
 package com.dicoding.caritahu.data.network
 
-import com.dicoding.caritahu.data.network.model.Article
+import com.dicoding.caritahu.data.network.model.HoaxArticle
+import com.dicoding.caritahu.data.network.model.HoaxAuthor
+import com.dicoding.caritahu.data.network.model.NewsArticle
+import com.dicoding.caritahu.data.network.model.NewsSource
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Endpoint {
     @GET("news/headlines")
-    suspend fun topHeadlines(): Response<List<Article>>
+    suspend fun newsHeadlines(): Response<List<NewsArticle>>
+
+    @GET("news/search/{q}")
+    suspend fun newsSearch(@Path("q") q: String): Response<List<NewsArticle>>
+
+    @GET("news/sources")
+    suspend fun newsSources(): Response<List<NewsSource>>
+
+    @GET("hoax/latest")
+    suspend fun hoaxLatest(): Response<List<HoaxArticle>>
+
+    @GET("hoax/search/{q}")
+    suspend fun hoaxSearch(@Path("q") q: String): Response<List<HoaxArticle>>
+
+    @GET("hoax/authors")
+    suspend fun hoaxAuthors(): Response<List<HoaxAuthor>>
 }
