9da27779007d95eebfa2449fb0bd1e5d5710c6aa
Unero <ramadhanbhg@gmail.com>
2021-12-10 00:27:05 +0700

    refactor: rename SplashScreen to SplashActivity

--- app/src/main/java/com/dicoding/caritahu/SplashActivity.kt
+++ app/src/main/java/com/dicoding/caritahu/SplashActivity.kt
@@ -1,66 +1,60 @@
 package com.dicoding.caritahu
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
+import androidx.lifecycle.lifecycleScope
+import com.dicoding.caritahu.databinding.ActivitySplashBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-
-@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
 
-        hideSystemUI()
-        actionBar?.hide()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val topSplash: ImageView = findViewById(R.id.top_splash)
-        val bottomSplash: ImageView = findViewById(R.id.bottom_splash)
         val topAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
         val bottomAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
 
-        topSplash.animation = topAnim
-        bottomSplash.animation = bottomAnim
+        binding.apply {
+            topSplash.animation = topAnim
+            bottomSplash.animation = bottomAnim
+        }
 
-        launchIntent()
+        goToMain()
     }
 
-    private fun launchIntent() {
-        CoroutineScope(Dispatchers.Default).launch {
+    private fun goToMain(){
+        lifecycleScope.launch(Dispatchers.Main) {
             delay(5000)
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
         }
     }
 
-    private fun hideSystemUI() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-            window.insetsController?.let {
-                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
-        }
-    }
+//    private fun hideSystemUI() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            window.setDecorFitsSystemWindows(false)
+//            window.insetsController?.let {
+//                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+//                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//            }
+//        } else {
+//            @Suppress("DEPRECATION")
+//            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+//                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+//        }
+//    }
 }
